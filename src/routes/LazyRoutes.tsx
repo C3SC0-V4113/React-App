f871e37549c66c267627d592c2ef5d3c03bc5865
CESCO <cescovalledev@gmail.com>
2023-10-30 22:34:07 -0600

    webpackChunk?

--- src/routes/LazyRoutes.tsx
+++ src/routes/LazyRoutes.tsx
@@ -1,9 +1,18 @@
 import { RouteObject, Navigate } from "react-router-dom";
 import { lazy } from "react";
 
-const Lazy1 = await lazy(() => import("../01-lazyload/pages/LazyPage1"));
-const Lazy2 = await lazy(() => import("../01-lazyload/pages/LazyPage2"));
-const Lazy3 = await lazy(() => import("../01-lazyload/pages/LazyPage3"));
+const Lazy1 = await lazy(
+  () =>
+    import(/*webpackChunkName: "LazyPage1"*/ "../01-lazyload/pages/LazyPage1")
+);
+const Lazy2 = await lazy(
+  () =>
+    import(/*webpackChunkName: "LazyPage2"*/ "../01-lazyload/pages/LazyPage2")
+);
+const Lazy3 = await lazy(
+  () =>
+    import(/*webpackChunkName: "LazyPage3"*/ "../01-lazyload/pages/LazyPage3")
+);
 
 export const LazyRoutes: RouteObject[] = [
   {
