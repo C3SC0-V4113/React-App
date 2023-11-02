06e6a821d9525d3a99f17564551da6a74dd806b7
CESCO <cescovalledev@gmail.com>
2023-11-01 23:10:20 -0600

    Product Card done the usual way

--- src/02-component-pattern/components/ProductCard.tsx
+++ src/02-component-pattern/components/ProductCard.tsx
@@ -2,18 +2,27 @@ import styles from "../styles/styles.module.css";
 import noImage from "../assets/no-image.jpg";
 import { useProducts } from "../hooks/useProducts";
 
-export const ProductCard = () => {
+interface Props {
+  product: Product;
+}
+
+interface Product {
+  id: string;
+  title: string;
+  img?: string;
+}
+
+export const ProductCard = ({ product }: Props) => {
   const { counter, increaseBy } = useProducts();
 
   return (
     <div className={styles.productCard}>
       <img
         className={styles.productImg}
-        src="./coffee-mug.png"
-        alt="Coffee Mug"
+        src={product.img ? product.img : noImage}
+        alt={product.id + product.title}
       />
-      {/* <img className={styles.productImg} src={noImage} alt="placeholder" /> */}
-      <span className={styles.productDescription}>Coffee Mug</span>
+      <span className={styles.productDescription}>{product.title}</span>
       <div className={styles.buttonsContainer}>
         <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>
           -
