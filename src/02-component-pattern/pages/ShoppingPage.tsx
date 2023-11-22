04320aab27600370c2127a4bb7e40fc3defa1d13
CESCO <cescovalledev@gmail.com>
2023-11-21 21:34:08 -0600

    using custom Hook for better code

--- src/02-component-pattern/pages/ShoppingPage.tsx
+++ src/02-component-pattern/pages/ShoppingPage.tsx
@@ -1,79 +1,14 @@
-import { useState } from "react";
+import "../styles/custom-styles.css";
 import {
   ProductButtons,
   ProductCard,
   ProductImage,
   ProductTitle,
 } from "../components";
-import { Product } from "../interfaces/interfaces";
-
-import "../styles/custom-styles.css";
-
-const product1 = {
-  id: "1",
-  title: "Coffee Mug - Card",
-  img: "./coffee-mug.png",
-};
-
-const product2 = {
-  id: "2",
-  title: "Coffee Mug - Meme",
-  img: "./coffee-mug2.png",
-};
-
-const products: Product[] = [product1, product2];
-
-interface ProductInCart extends Product {
-  count: number;
-}
+import { useShoppingCart } from "../hooks/useShoppingCart";
 
 export const ShoppingPage = () => {
-  const [shoppingCart, setShoppingCart] = useState<{
-    [key: string]: ProductInCart;
-  }>({});
-
-  const onProductCountChange = ({
-    count,
-    product,
-  }: {
-    count: number;
-    product: Product;
-  }) => {
-    // console.log({ count });
-
-    setShoppingCart((oldShoppingCart) => {
-      const productInCart: ProductInCart = oldShoppingCart[product.id] || {
-        ...product,
-        count: 0,
-      };
-
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-        return {
-          ...oldShoppingCart,
-          [product.id]: productInCart,
-        };
-      }
-
-      const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-
-      console.log("Deleted: ", toDelete);
-      // Borrar el producto
-      return rest;
-
-      // if (count === 0) {
-      // const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-
-      // console.log("Deleted: ", toDelete);
-      // return rest;
-      // }
-
-      // return {
-      //   ...oldShoppingCart,
-      //   [product.id]: { ...product, count },
-      // };
-    });
-  };
+  const { products, onProductCountChange, shoppingCart } = useShoppingCart();
 
   return (
     <div>
