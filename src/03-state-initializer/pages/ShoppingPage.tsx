22aa436b83303c13861db249f37cc7d947b8fe32
CESCO <cescovalledev@gmail.com>
2023-11-25 21:20:34 -0600

    prepare deploying to npm

--- src/03-state-initializer/pages/ShoppingPage.tsx
+++ src/03-state-initializer/pages/ShoppingPage.tsx
@@ -6,8 +6,6 @@ import {
 } from "../components";
 import { products } from "../data/products";
 
-import "../styles/custom-styles.css";
-
 const product = products[0];
 
 export const ShoppingPage = () => {
@@ -19,27 +17,16 @@ export const ShoppingPage = () => {
       <ProductCard
         key={product.id}
         product={product}
-        className="bg-dark text-white"
         initialValues={{
           count: 4,
-          maxCount: 10,
+          // maxCount: 10,
         }}
       >
-        {({ reset, count, increaseBy, isMaxCountReached, maxCount }) => (
+        {() => (
           <>
-            <ProductImage className="custom-image" />
-            <ProductTitle className="text-bold" />
-            <ProductButtons className="custom-buttons" />
-            <button onClick={reset}>Reset</button>
-            <button onClick={() => increaseBy(-2)}>-2</button>
-            {/* Si no se llega al maxCount, ocultar */}
-            {!isMaxCountReached && (
-              <button onClick={() => increaseBy(2)}>+2</button>
-            )}
-
-            <span>
-              {count} - {maxCount}
-            </span>
+            <ProductImage />
+            <ProductTitle />
+            <ProductButtons />
           </>
         )}
       </ProductCard>
