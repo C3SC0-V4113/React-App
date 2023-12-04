3576825e6b8eb70cbdbb81e8c2ae140971761901
CESCO <cescovalledev@gmail.com>
2023-12-04 16:43:26 -0600

    RegisterPage begins

--- src/04-forms/pages/FormsPages.tsx
+++
@@ -1,3 +0,0 @@
-export const FormsPages = () => {
-  return <div>FormsPages</div>;
-};
--- src/04-forms/routes/FormRoutes.tsx
+++ src/04-forms/routes/FormRoutes.tsx
@@ -1,9 +1,10 @@
 import { RouteObject } from "react-router-dom";
-import { FormsPages } from "../pages/FormsPages";
+// import { FormsPages } from "../pages/FormsPages";
+import RegisterPage from "../pages/RegisterPage";
 
 export const FormRoutes: RouteObject[] = [
   {
     index: true,
-    element: <FormsPages />,
+    element: <RegisterPage />,
   },
 ];
