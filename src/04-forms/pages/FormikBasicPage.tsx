ab0c495de916a3145569faafde07be456f6b1d24
CESCO <cescovalledev@gmail.com>
2023-12-05 14:54:46 -0600

    beggining formik example

--- src/04-forms/pages/FormikBasicPage.tsx
+++ src/04-forms/pages/FormikBasicPage.tsx
@@ -1,18 +1,47 @@
+import { useFormik } from "formik";
+import "../styles/styles.css";
+
 const FormikBasicPage = () => {
+  const { handleChange, values, handleSubmit } = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <div>
       <h1>Formik Basic Tutorial</h1>
-      <form noValidate>
+      <form onSubmit={handleSubmit} noValidate>
         <label htmlFor="firstName">First Name</label>
-        <input type="text" name="firstName" />
+        <input
+          type="text"
+          name="firstName"
+          onChange={handleChange}
+          value={values.firstName}
+        />
         <span>First Name is required</span>
 
         <label htmlFor="lastName">Last Name</label>
-        <input type="text" name="lastName" />
+        <input
+          type="text"
+          name="lastName"
+          onChange={handleChange}
+          value={values.lastName}
+        />
         <span>Last Name is required</span>
 
         <label htmlFor="email">Email Address</label>
-        <input type="email" name="email" />
+        <input
+          type="email"
+          name="email"
+          onChange={handleChange}
+          value={values.email}
+        />
         <span>Email is required</span>
         <span>Invalid email</span>
 
